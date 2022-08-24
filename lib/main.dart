5e2ea010eb4f85b1944edeba7437b12cbf110bbf
ammarIbrahim <ammaribrahim5955@gmail.com>
2022-08-24 07:51:02 +0200

    comment out delete button "needs more work"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -115,15 +115,15 @@ class _FirstScreenState extends State<FirstScreen> {
                     return ListTile(
                       title: Text(snapshot.data!.docs[index]['name']),
                       subtitle: Text(snapshot.data!.docs[index]['phone']),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        color: Colors.red,
-                        onPressed: () {
-                          setState(() {
-                            snapshot.data!.docs.removeAt(index);
-                          });
-                        },
-                      ),
+                      // trailing: IconButton(
+                      //   icon: const Icon(Icons.delete),
+                      //   color: Colors.red,
+                      //   onPressed: () {
+                      //     setState(() {
+                      //       snapshot.data!.docs.removeAt(index);
+                      //     });
+                      //   },
+                      // ),
                     );
                   },
                 )
